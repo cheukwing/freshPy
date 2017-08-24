db93ac3a43c95b7487a7b3964dec77e8f07adab7
Michael Tsang <michaeltsang1998@gmail.com>
2017-08-24 18:18:57 +0100

    remove prints when no fresh tracks

--- fresh.py
+++ fresh.py
@@ -9,18 +9,17 @@ def search():
   for artist in artists_file:
     hasFresh = False
     names = artist.split(' / ')
-    print names[0].upper().rstrip() + ":"
     
     for name in names:
       for submission in reddit.subreddit('hiphopheads').search(
                           name, time_filter="week"):
         title = submission.title
         if "[FRESH" in title:
+          if not hasFresh:
+            print names[0].upper().rstrip() + ":"
+
           print "  * " + title
           hasFresh = True
-
-    if not hasFresh:
-      print "Nothing!"
 
 def main():
   search()
